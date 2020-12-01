3a756992c02edb7a320c2f1b684d42eb9df76fe0
yanli <yanli@altstory.com>
2020-12-01 16:12:02 +0800

    完成了登陆功能

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 
+// 导入图标
+import './assets/font/iconfont.css'
+// 导入全局样式表
+import './assets/css/global.css'
+
+// 配置axios
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8080/api/'
+Vue.prototype.$http = axios
+
 Vue.config.productionTip = false
 
 new Vue({
